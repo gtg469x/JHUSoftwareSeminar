b0ef309711b0fd81279e31de27233ead840ed7b8
gtg469x <caldwell.kristinl@gmail.com>
2012-10-08 21:26:49 -0500

    adding the com folder with the controller

---
+++ src/com/SeminarController.java
@@ -0,0 +1,115 @@
+package com;
+
+import registration.Register;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SeminarController
+ */
+@WebServlet("/SeminarController")
+public class SeminarController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SeminarController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+	
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.setContentType("text/html;charset=UTF-8");
+		HttpSession session = request.getSession(true);
+		
+		String errorMessage = "";
+        ServletContext servletContext = getServletContext();
+        ServletConfig servletConfig = getServletConfig();
+        
+
+        //validation checks on required parameters.  Forwards error to registrationError Page.
+        if(request.getParameterValues("courseListings")==null){
+        	errorMessage = "You have not entered any courses, Please go back and select courses to register for";
+        	request.setAttribute("error", errorMessage);
+        	RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/registrationError.jsp");
+        	dispatcher.forward(request, response);
+        }else if(request.getParameter("empStatus")==null){
+        	errorMessage = "You have not entered your employment status.  Please go back and select your Employment Status";
+        	request.setAttribute("error", errorMessage);
+        	RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/registrationError.jsp");
+        	dispatcher.forward(request, response);
+        }else{
+        	errorMessage = "";
+        	
+	        //get necessary parameters for processing
+	        String name = request.getParameter("name");
+	        String [] courses = request.getParameterValues("courseListings");
+	        String status = request.getParameter("empStatus");
+	        String email = request.getParameter("email");
+	        boolean hotel = false;
+	        boolean parking = false;
+	        
+	      //determine boolean values for hotel and parking fees
+	        if(request.getParameter("hotel")!=null){
+	        	hotel = true;
+	        	
+	        }
+	
+	        if(request.getParameter("parking")!=null){
+	        	parking = true;
+	        }
+	
+	      //determine String Equivalent for the value associated with status Parameter
+	        if(status.equals("jhuEmp")){
+	        	status="JHU Employee";
+	        }else if(status.equals("jhuStud")){
+	        	status="JHU Student";
+	        }else if(status.equals("speaker")){
+	        	status="Speaker";
+	        }else{
+	        	status="Other";
+	        }
+	        
+	        Register regUser = new Register(name, email, status);
+	        regUser.calculateCourseFees(courses);
+	        regUser.calculateTotalFees(courses, hotel, parking);
+	        
+	        session.setAttribute("coursesLength", courses.length);
+	      
+	        
+	    	
+	        RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/registrationCosts.jsp");
+	    	dispatcher.forward(request, response);
+	        
+		}
+		
+	}
+		
+
+}
