70ae2b089db9767b803ff01a0b866acd95d09ee0
gtg469x <caldwell.kristinl@gmail.com>
2012-10-08 20:21:08 -0500

    Second Commit with updates using javabean and servlet

--- WebContent/registrationCosts.jsp
+++ WebContent/registrationCosts.jsp
@@ -4,12 +4,44 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<jsp:useBean id="rcb" scope="session" class="registration.RegCostBean"/>
 <link rel="stylesheet" type="text/css" href="jhuStyle.css">
+<%@ page errorPage="registrationError.jsp" %>
 <title>Registration Successful</title>
  <img src="pics/jhuLogo.jpg">
 </head>
 <body>
 <h1>Johns Hopkins Annual Software Development Seminar</h1>
+<h2><jsp:getProperty name="rcb" property="welcomeName"/></h2>
+<br>
+<br>
+You are registered as a <b><jsp:getProperty name="rcb" property="registrationType"/></b>
+<br>
+<br>
+Your email confirmation will be sent to: <b><jsp:getProperty name="rcb" property="email"/></b>
+<br>
+<br>
+<table frame="below" width="400" rules="rows">
+<tr><td align="center"><i><b>Your Courses</b></i></td><td align="right"><i><b>Costs</b></i></td></tr>
+<%
+/*
+for(int i=0; i<Integer.parseInt((String) session.getAttribute("coursesLength")); i++){
+	out.println("<tr><td>"+courses[i]+"\t</td><td align=\"right\">\t$"++"</td></tr>");
+}
+if(parking){
+	out.println("<tr><td>"+"Parking Fees"+"\t</td><td align=\"right\">\t$"+regUser.calculateParkingFee()+"</td></tr>");
+}
+if(hotel){
+	out.println("<tr><td>"+"Hotel Accommodations"+"\t</td><td align=\"right\">\t$"+regUser.calculateHotelFee()+"</td></tr>");
+}
+*/
 
+%>	
+	<tr><td align="right"><b>Total:</b></td><td align="right"><b>$<jsp:getProperty name="rcb" property="totalCost"/></b></td></tr>
+</table>
+
+<form action="seminarRegistration.html" method="get">
+  <input type="submit" value="Back">
+</form>
 </body>
 </html>
--- src/registration/Register.java
+++ src/registration/Register.java
@@ -1,16 +1,35 @@
 package registration;
 
-public class Register {
+import java.io.Serializable;
 
+public class Register implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private User registerUser;
 	private Fees charges;
 	private double totalCost;
 	private double individualCourseCost;
+	private String welcomeName;
+	private String registrationType;
+	private String email;
+	RegCostBean rcb;
+	
 	
 	public Register(String userName, String userEmail, String userStatus){
 		registerUser = new User(userName, userEmail, userStatus);
+		rcb = new RegCostBean();
 		charges = new Fees();
 		totalCost=0;
+		
+		setRegType();
+		setWelcomeName();
+		setFees();
+		confirmationEmail();
+		setIndividualCourseCost();
+		
 	}
 	
 	public double calculateCourseFees(String [] courses){
@@ -41,32 +60,47 @@ public class Register {
 		return charges.getHotelFee();
 	}
 	
-	public double calculateTotalFees(String [] courses, boolean hotel, boolean parking){
+	public void setFees(){
+		rcb.setHotelFee(charges.getHotelFee());
+		rcb.setParkingFee(charges.getParkingFee());
+	}
+	
+	public void calculateTotalFees(String [] courses, boolean hotel, boolean parking){
 		if(hotel&&parking){
 			totalCost = calculateHotelFee()+calculateCourseFees(courses)+calculateParkingFee();
 		}else if(hotel&&!parking){
 			totalCost = calculateHotelFee()+calculateCourseFees(courses);
 		}else if(!hotel&&parking){
 			totalCost = calculateCourseFees(courses)+calculateParkingFee();
+		}else{
+			totalCost=calculateCourseFees(courses);
 		}
-			return totalCost;
+			rcb.setTotalCost(totalCost);
 	}
 	
-	public String registrationType(){
-		return registerUser.getUserStatus();
+	private void setRegType(){
+		rcb.setRegistrationType(registerUser.getUserStatus());
+	}
+		
+	private void confirmationEmail(){
+		rcb.setEmail(registerUser.getUserEmail());
 	}
 	
-	public String confirmationEmail(){
-		return registerUser.getUserEmail();
+	private void setWelcomeName(){
+		rcb.setWelcomeName(registerUser.getUserName());
 	}
-	
-	public String welcomeName(){
-		return registerUser.getUserName();
-	}
-	
-	public double getIndividualCourseCost(){
-		return this.individualCourseCost;
-	}
-	
+
+	private void setIndividualCourseCost(){
+		if(registerUser.isEmployee()){
+			individualCourseCost = charges.getCourseEmployeeFee();
+		}else if(registerUser.isStudent()){
+			individualCourseCost=charges.getCourseStudentFee();
+		}else if(registerUser.isSpeaker()){
+			individualCourseCost=charges.getCourseSpeakerFee();
+		}else{
+			individualCourseCost=charges.getCourseOtherFee();
+		}
+		rcb.setIndividualCost(individualCourseCost);
+	}	
 	
 }
